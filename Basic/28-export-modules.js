dc69579c8ec882d22601f908b593610de9fdc132
jafprofesor <124161700+jafprofesor@users.noreply.github.com>
2025-02-27 13:47:40 +0100

    Update 28-export-modules.js

--- Basic/28-export-modules.js
+++ Basic/28-export-modules.js
@@ -1,6 +1,5 @@
 /*
 Clase 44 - Módulos
-Vídeo: https://youtu.be/1glVfFxj8a4?t=21480
 */
 
 // Exportación de módulos
@@ -16,7 +15,7 @@ console.log(add(5, 10))
 // Propiedades
 
 export const PI = 3.1416
-export let name = "MoureDev"
+export let name = "jaf"
 
 // Clases
 
